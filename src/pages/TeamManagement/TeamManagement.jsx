beb229bb6a645c02ba1ca6dc6e6c3cde41fb399d
Sumaiya Sinja <sumiyasinja@gmail.com>
2024-05-14 23:48:23 +0600

    team management page designed

--- src/pages/TeamManagement/TeamManagement.jsx
+++ src/pages/TeamManagement/TeamManagement.jsx
@@ -3,19 +3,16 @@ const TeamManagement = () => {
   return (
     <div className="">
       <div className="container mx-auto">
-      <button
-        
-        className=" text-white text-lg  py-2 px-2 lg:px-5 bg-light-green font-semibold rounded-3xl "
-      >
-        Create New User
-      </button>
-        <table className="table">
+        <div className=" flex justify-end">
+          <button className=" text-white text-lg  py-2 px-2 lg:px-5 bg-light-green font-semibold rounded-3xl ">
+            Create New User
+          </button>
+        </div>
+        <table className="table mt-5">
           {/* head */}
           <thead>
             <tr>
-              <th className="py-2 text-base text-white bg-light-green">
-                #SL
-              </th>
+              <th className="py-2 text-base text-white bg-light-green">#SL</th>
               <th className="py-2 text-base text-white bg-light-green">
                 | Name
               </th>
@@ -39,7 +36,7 @@ const TeamManagement = () => {
             {/* row 1 */}
             <tr>
               {/* <th>1</th> */}
-              
+
               <td>Cy Ganderton</td>
               <td>Quality Control Specialist</td>
               <td>Blue</td>
@@ -50,7 +47,7 @@ const TeamManagement = () => {
             {/* row 2 */}
             <tr>
               {/* <th>2</th> */}
-          
+
               <td>Hart Hagerty</td>
               <td>Desktop Support Technician</td>
               <td>Purple</td>
@@ -61,7 +58,7 @@ const TeamManagement = () => {
             {/* row 3 */}
             <tr>
               {/* <th>3</th> */}
-   
+
               <td>Brice Swyre</td>
               <td>Tax Accountant</td>
               <td>Red</td>
